9e906ff4e750a0ef12a7787872c560bc43f1ffaf
Warren Moore <wm@warrenmoore.net>
2024-01-09 13:23:46 -0800

    Bumped package version to 0.5.6 This release increases the Swift tools required version to 5.5 in order to support Mac Catalyst This release also adds support for tvOS

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
-// swift-tools-version:5.3
+//swift-tools-version: 5.5
 
 import PackageDescription
 
 let package = Package(
     name: "GLTFKit2",
     platforms: [
-        .macOS("11.0"), .iOS("12.1")
+        .macOS("11.0"), .macCatalyst("14.0"), .iOS("12.1"), .tvOS("12.1")
+        // Note: visionOS("1.0") is also supported, but we can't require Swift tools version 5.9 yet.
     ],
     products: [
         .library(name: "GLTFKit2",
@@ -13,7 +14,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "GLTFKit2",
-                      url: "https://github.com/warrenm/GLTFKit2/releases/download/0.5.5/GLTFKit2.xcframework.zip",
-                      checksum: "bd91c172d9d9395a550395e6729a139524d2d3e8823c069b836e681ce93b2a5a")
+                      url: "https://github.com/warrenm/GLTFKit2/releases/download/0.5.6/GLTFKit2.xcframework.zip",
+                      checksum:"e98889a41a00b528dcc3577d4d263202deb4a07d86f0066f45e01652c530fe96")
     ]
 )
