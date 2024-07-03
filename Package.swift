0e399286a29580a6f3c0f49aa01075b5c43a9ce2
Warren Moore <wm@warrenmoore.net>
2024-07-03 10:34:28 -0700

    Bumped package version to 0.5.11

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "GLTFKit2",
-                      url: "https://github.com/warrenm/GLTFKit2/releases/download/0.5.10/GLTFKit2.xcframework.zip",
-                      checksum:"ed3ab68ec150b23b933954e429307a429fb8fbce4bcf560f687e5b2b46fc2722")
+                      url: "https://github.com/warrenm/GLTFKit2/releases/download/0.5.11/GLTFKit2.xcframework.zip",
+                      checksum:"a3d62ba63ae946553acb0e3fb60b7183966841cd2c492ceb47fc6d5df227dab8")
     ]
 )
